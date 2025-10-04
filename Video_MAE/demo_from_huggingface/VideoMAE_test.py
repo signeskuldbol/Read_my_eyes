dbef542715e75cb93c8abb1e1f74f71805be6ea5
RasmusBJensen <117160777+RasmusBJensen@users.noreply.github.com>
2025-10-04 20:24:08 +0200

    cropped all videos in dataset. chanced the yolo face model to have moving average, fixed W,H and return a 224x224 framed video

---
+++ Video_MAE/demo_from_huggingface/VideoMAE_test.py
@@ -0,0 +1,236 @@
+from huggingface_hub import hf_hub_download
+import os
+
+hf_dataset_identifier = "sayakpaul/ucf101-subset"
+filename = "UCF101_subset.tar.gz"
+file_path = hf_hub_download(repo_id=hf_dataset_identifier, filename=filename, repo_type="dataset")
+
+import tarfile
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+
+with tarfile.open(file_path) as t:
+     t.extractall(".")
+
+batch_size = 4  # or 2, 8, etc. depending on your GPU
+import torch
+
+import pathlib
+from pathlib import Path
+import os
+folder_path = Path(__file__).parent
+dataset_root_path = folder_path / "UCF101_subset"
+dataset_root_path = pathlib.Path(dataset_root_path)
+video_count_train = len(list(dataset_root_path.glob("train/*/*.avi")))
+video_count_val = len(list(dataset_root_path.glob("val/*/*.avi")))
+video_count_test = len(list(dataset_root_path.glob("test/*/*.avi")))
+video_total = video_count_train + video_count_val + video_count_test
+print(f"Total videos: {video_total}")
+all_video_file_paths = (
+    list(dataset_root_path.glob("train/*/*.avi"))
+    + list(dataset_root_path.glob("val/*/*.avi"))
+    + list(dataset_root_path.glob("test/*/*.avi"))
+ )
+all_video_file_paths[:5]
+
+# Each file path looks like: .../<split>/<class>/<video>.avi
+# The class is simply the parent directory name.
+class_labels = sorted({p.parent.name for p in all_video_file_paths})
+label2id = {label: i for i, label in enumerate(class_labels)}
+id2label = {i: label for label, i in label2id.items()}
+print(f"Unique classes: {list(label2id.keys())}.")
+
+label2id = {label: i for i, label in enumerate(class_labels)}
+id2label = {i: label for label, i in label2id.items()}
+
+print(f"Unique classes: {list(label2id.keys())}.")
+
+
+from transformers import VideoMAEImageProcessor, VideoMAEForVideoClassification
+
+model_ckpt = "MCG-NJU/videomae-base"
+image_processor = VideoMAEImageProcessor.from_pretrained(model_ckpt)
+model = VideoMAEForVideoClassification.from_pretrained(
+    model_ckpt,
+    label2id=label2id,
+    id2label=id2label,
+    ignore_mismatched_sizes=True,  # provide this in case you're planning to fine-tune an already fine-tuned checkpoint
+)
+
+import pytorchvideo.data
+
+from pytorchvideo.transforms import (
+    ApplyTransformToKey,
+    Normalize,
+    RandomShortSideScale,
+    RemoveKey,
+    ShortSideScale,
+    UniformTemporalSubsample,
+)
+
+from torchvision.transforms import (
+    Compose,
+    Lambda,
+    RandomCrop,
+    RandomHorizontalFlip,
+    Resize,
+)
+mean = image_processor.image_mean
+std = image_processor.image_std
+if "shortest_edge" in image_processor.size:
+    height = width = image_processor.size["shortest_edge"]
+else:
+    height = image_processor.size["height"]
+    width = image_processor.size["width"]
+resize_to = (height, width)
+
+num_frames_to_sample = model.config.num_frames
+sample_rate = 4
+fps = 30
+clip_duration = num_frames_to_sample * sample_rate / fps
+
+train_transform = Compose(
+    [
+        ApplyTransformToKey(
+            key="video",
+            transform=Compose(
+                [
+                    UniformTemporalSubsample(num_frames_to_sample),
+                    Lambda(lambda x: x / 255.0),
+                    Normalize(mean, std),
+                    RandomShortSideScale(min_size=256, max_size=320),
+                    RandomCrop(resize_to),
+                    RandomHorizontalFlip(p=0.5),
+                ]
+            ),
+        ),
+    ]
+)
+
+train_dataset = pytorchvideo.data.Ucf101(
+    data_path=os.path.join(dataset_root_path, "train"),
+    clip_sampler=pytorchvideo.data.make_clip_sampler("random", clip_duration),
+    decode_audio=False,
+    transform=train_transform,
+    decoder="decord",
+)
+
+val_transform = Compose(
+    [
+        ApplyTransformToKey(
+            key="video",
+            transform=Compose(
+                [
+                    UniformTemporalSubsample(num_frames_to_sample),
+                    Lambda(lambda x: x / 255.0),
+                    Normalize(mean, std),
+                    Resize(resize_to),
+                ]
+            ),
+        ),
+    ]
+)
+
+val_dataset = pytorchvideo.data.Ucf101(
+    data_path=os.path.join(dataset_root_path, "val"),
+    clip_sampler=pytorchvideo.data.make_clip_sampler("uniform", clip_duration),
+    decode_audio=False,
+    transform=val_transform,
+    decoder="decord",
+)
+
+test_dataset = pytorchvideo.data.Ucf101(
+    data_path=os.path.join(dataset_root_path, "test"),
+    clip_sampler=pytorchvideo.data.make_clip_sampler("uniform", clip_duration),
+    decode_audio=False,
+    transform=val_transform,
+    decoder="decord",
+)
+
+print(train_dataset.num_videos, val_dataset.num_videos, test_dataset.num_videos)
+
+import imageio
+import numpy as np
+from IPython.display import Image
+
+def unnormalize_img(img):
+    """Un-normalizes the image pixels."""
+    img = (img * std) + mean
+    img = (img * 255).astype("uint8")
+    return img.clip(0, 255)
+
+def create_gif(video_tensor, filename="sample.gif"):
+    """Prepares a GIF from a video tensor.
+    The video tensor is expected to have the following shape:
+    (num_frames, num_channels, height, width).
+    """
+    frames = []
+    for video_frame in video_tensor:
+        frame_unnormalized = unnormalize_img(video_frame.permute(1, 2, 0).numpy())
+        frames.append(frame_unnormalized)
+    kargs = {"duration": 0.25}
+    imageio.mimsave(filename, frames, "GIF", **kargs)
+    return filename
+
+def display_gif(video_tensor, gif_name="sample.gif"):
+    """Prepares and displays a GIF from a video tensor."""
+    video_tensor = video_tensor.permute(1, 0, 2, 3)
+    gif_filename = create_gif(video_tensor, gif_name)
+    return Image(filename=gif_filename)
+
+sample_video = next(iter(train_dataset))
+video_tensor = sample_video["video"]
+display_gif(video_tensor)
+
+from transformers import TrainingArguments, Trainer
+
+model_name = model_ckpt.split("/")[-1]
+new_model_name = f"{model_name}-finetuned-ucf101-subset"
+num_epochs = 4
+
+args = TrainingArguments(
+    new_model_name,
+    remove_unused_columns=False,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=5e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    warmup_ratio=0.1,
+    logging_steps=10,
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy",
+    push_to_hub=True,
+    max_steps=(train_dataset.num_videos // batch_size) * num_epochs,
+)
+
+import evaluate
+
+metric = evaluate.load("accuracy")
+
+
+def compute_metrics(eval_pred):
+    predictions = np.argmax(eval_pred.predictions, axis=1)
+    return metric.compute(predictions=predictions, references=eval_pred.label_ids)
+
+def collate_fn(examples):
+    # permute to (num_frames, num_channels, height, width)
+    pixel_values = torch.stack(
+        [example["video"].permute(1, 0, 2, 3) for example in examples]
+    )
+    labels = torch.tensor([example["label"] for example in examples])
+    return {"pixel_values": pixel_values, "labels": labels}
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    processing_class=image_processor,
+    compute_metrics=compute_metrics,
+    data_collator=collate_fn,
+)
+
+train_results = trainer.train()
